fd75dd81845dcd0e7e194f45d085812902ec36b7
Javad Esmaeili <javadesmaeili1388@gmail.com>
2020-12-31 15:15:26 +0330

    api.php update
    Routes method name syntax

--- routes/api.php
+++ routes/api.php
@@ -28,54 +28,46 @@ Route::prefix('admin')->middleware([CheckHeaders::class])->group(function () {
 
     Route::middleware([CheckToken::class])->group(function () {
         Route::post('/admins', [Person::class, 'admins'])->name('admins');
-        Route::post('/adminroles', [Person::class, 'adminRoles'])->name('adminRoles');
-        Route::post('/createperson', [Person::class, 'createPerson'])->name('createPerson');
-        Route::post('/editperson', [Person::class, 'editPerson'])->name('editPerson');
+        Route::post('/adminRoles', [Person::class, 'adminRoles'])->name('adminRoles');
+        Route::post('/createPerson', [Person::class, 'createPerson'])->name('createPerson');
+        Route::post('/editPerson', [Person::class, 'editPerson'])->name('editPerson');
         Route::post('/roles', [Person::class, 'roles'])->name('roles');
-        Route::post('/rolepermissions', [Person::class, 'rolePermissions'])->name('rolePermissions');
-        Route::post('/addpermission', [Person::class, 'addPermission'])->name('addPermission');
-        Route::post('/missingpermissions', [Person::class, 'missingPermissions'])->name('missingPermissions');
-        Route::post('/addrole', [Person::class, 'addRole'])->name('addRole');
-        Route::post('/deleterole', [Person::class, 'deleteRole'])->name('deleteRole');
-        Route::post('/editrole', [Person::class, 'editRole'])->name('editRole');
-        Route::post('/deletepermission', [Person::class, 'deletePermission'])->name('deletePermission');
-
-
+        Route::post('/rolePermissions', [Person::class, 'rolePermissions'])->name('rolePermissions');
+        Route::post('/addPermission', [Person::class, 'addPermission'])->name('addPermission');
+        Route::post('/missingPermissions', [Person::class, 'missingPermissions'])->name('missingPermissions');
+        Route::post('/addRole', [Person::class, 'addRole'])->name('addRole');
+        Route::post('/deleteRole', [Person::class, 'deleteRole'])->name('deleteRole');
+        Route::post('/editRole', [Person::class, 'editRole'])->name('editRole');
+        Route::post('/deletePermission', [Person::class, 'deletePermission'])->name('deletePermission');
 
         Route::post('/saveFile', [FileManager::class, 'saveFile'])->name('saveFile');
         Route::post('/saveFiles', [FileManager::class, 'saveFiles'])->name('saveFiles');
         Route::post('/deleteFile', [FileManager::class, 'deleteFile'])->name('deleteFile');
         Route::post('/deleteFiles', [FileManager::class, 'deleteFiles'])->name('deleteFiles');
 
-
-
-
-
-
-        Route::post('/createpost', [Post::class, 'createPost'])->name('createPost');
-        Route::post('/postlist', [Post::class, 'postList'])->name('postList');
-        Route::post('/changestatus', [Post::class, 'changeStatus'])->name('changeStatus');
-        Route::post('/deletepost', [Post::class, 'deletePost'])->name('deletePost');
-        Route::post('/editpost', [Post::class, 'editPost'])->name('editPost');
-        Route::post('/createmaincategory', [Category::class, 'createMainCategory'])->name('createMainCategory');
-        Route::post('/maincategorylist', [Category::class, 'mainCategoryList'])->name('mainCategoryList');
-        Route::post('/editmaincategory', [Category::class, 'editMainCategory'])->name('editMainCategory');
-        Route::post('/deletemainctegory', [Category::class, 'deleteMainCtegory'])->name('deleteMainCtegory');
-        Route::post('/createbubcategory', [Category::class, 'createSubCategory'])->name('createSubCategory');
-        Route::post('/subcategorylist', [Category::class, 'subCategoryList'])->name('subCategoryList');
+        Route::post('/createPost', [Post::class, 'createPost'])->name('createPost');
+        Route::post('/postList', [Post::class, 'postList'])->name('postList');
+        Route::post('/changeStatus', [Post::class, 'changeStatus'])->name('changeStatus');
+        Route::post('/deletePost', [Post::class, 'deletePost'])->name('deletePost');
+        Route::post('/editPost', [Post::class, 'editPost'])->name('editPost');
+        Route::post('/createMainCategory', [Category::class, 'createMainCategory'])->name('createMainCategory');
+        Route::post('/mainCategoryList', [Category::class, 'mainCategoryList'])->name('mainCategoryList');
+        Route::post('/editMainCategory', [Category::class, 'editMainCategory'])->name('editMainCategory');
+        Route::post('/deleteMainCtegory', [Category::class, 'deleteMainCtegory'])->name('deleteMainCtegory');
+        Route::post('/createSubCategory', [Category::class, 'createSubCategory'])->name('createSubCategory');
+        Route::post('/subCategoryList', [Category::class, 'subCategoryList'])->name('subCategoryList');
         Route::post('/editsubcategory', [Category::class, 'editsubcategory'])->name('editsubcategory');
-        Route::post('/deletesubctegory', [Category::class, 'deleteSubCtegory'])->name('deleteSubCtegory');
-        Route::post('/createproduct', [Product::class, 'createProduct'])->name('createProduct');
-        Route::post('/productlist', [Product::class, 'productList'])->name('productList');
-        Route::post('/productlist', [Product::class, 'productList'])->name('productList');
-        Route::post('/deleteproduct', [Product::class, 'deleteProduct'])->name('deleteProduct');
-        Route::post('/editproducinfo', [Product::class, 'editProducInfo'])->name('editProducInfo');
-        Route::post('/productimagelist', [Product::class, 'productImageList'])->name('productImageList');
-        Route::post('/deleteproductimage', [Product::class, 'deleteProductImage'])->name('deleteProductImage');
-        Route::post('/addproductimage', [Product::class, 'addProductImage'])->name('addProductImage');
-        Route::post('/postcategorylist', [Category::class, 'postCategoryList'])->name('postCategoryList');
-        Route::post('/createpostcategory', [Category::class, 'createPostCategory'])->name('createPostCategory');
-        Route::post('/editpostcategory', [Category::class, 'editPostCategory'])->name('editPostCategory');
-        Route::post('/changedata', [Home::class, 'changeData'])->name('changeData');
+        Route::post('/deleteSubCtegory', [Category::class, 'deleteSubCtegory'])->name('deleteSubCtegory');
+        Route::post('/createProduct', [Product::class, 'createProduct'])->name('createProduct');
+        Route::post('/productList', [Product::class, 'productList'])->name('productList');
+        Route::post('/deleteProduct', [Product::class, 'deleteProduct'])->name('deleteProduct');
+        Route::post('/editProducInfo', [Product::class, 'editProducInfo'])->name('editProducInfo');
+        Route::post('/productImageList', [Product::class, 'productImageList'])->name('productImageList');
+        Route::post('/deleteProductImage', [Product::class, 'deleteProductImage'])->name('deleteProductImage');
+        Route::post('/addProductImage', [Product::class, 'addProductImage'])->name('addProductImage');
+        Route::post('/postCategoryList', [Category::class, 'postCategoryList'])->name('postCategoryList');
+        Route::post('/createPostCategory', [Category::class, 'createPostCategory'])->name('createPostCategory');
+        Route::post('/editPostCategory', [Category::class, 'editPostCategory'])->name('editPostCategory');
+        Route::post('/changeData', [Home::class, 'changeData'])->name('changeData');
     });
 });
